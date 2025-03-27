312b39fe9c0fed9dceb5d49b456fe45d6c36ecb3
Ezihe Godswill <ezihegodswill01@gmail.com>
2025-03-27 14:07:15 +0100

    refactor toasts components to match expected logic

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import Toast from '../Toast';
-import styles from './ToastShelf.module.css';
+import Toast from "../Toast";
+import { useToast } from "../ToastProvider";
+
+import styles from "./ToastShelf.module.css";
 
 function ToastShelf() {
+  const { toasts, removeToast } = useToast();
+
   return (
-    <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+    <ol
+      role="region"
+      aria-live="polite"
+      aria-label="Notification"
+      className={styles.wrapper}
+    >
+      {toasts.map(({ id, message, variant }) => (
+        <li key={id} className={styles.toastWrapper}>
+          <Toast variant={variant} dismiss={() => removeToast(id)}>
+            {message}
+          </Toast>
+        </li>
+      ))}
     </ol>
   );
 }
